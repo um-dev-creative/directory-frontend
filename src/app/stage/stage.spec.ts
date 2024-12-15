4b733898f11a38f104fcd52659c1603a6d4dcd46
Luis Mata <69470188+lanmata@users.noreply.github.com>
2024-12-15 07:30:09 -0500

    - Added Rose-Red material theme - Updated application setting in the app.config.ts file - Added default route in the app.routes.ts file -  Added international properties - Created Footer component - Created Header component

--- src/app/stage/stage.spec.ts
+++ src/app/stage/stage.spec.ts
@@ -1,30 +1,24 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {Stage} from './stage';
-import {of} from "rxjs";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 describe('Stage', () => {
-    let component: Stage;
-    let fixture: ComponentFixture<Stage>;
-    let mockStore: any;
+  let component: Stage;
+  let fixture: ComponentFixture<Stage>;
 
-    beforeEach(async () => {
-        mockStore = {
-            select: jasmine.createSpy().and.returnValue(of({})),
-            dispatch: jasmine.createSpy()
-        };
-        await TestBed.configureTestingModule({
-            imports: [Stage, BrowserAnimationsModule],
-        })
-            .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Stage, BrowserAnimationsModule],
+    })
+      .compileComponents();
 
-        fixture = TestBed.createComponent(Stage);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+    fixture = TestBed.createComponent(Stage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
