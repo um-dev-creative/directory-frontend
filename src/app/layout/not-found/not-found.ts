1168625a9553e118b490c56bb3570f307b4ca84a
omairys <hello.omug@gmail.com>
2025-06-04 12:54:27 -0400

    update not-found page

--- src/app/layout/not-found/not-found.ts
+++ src/app/layout/not-found/not-found.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Button } from '@app/components/ui';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [CommonModule, RouterModule, Button],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css'
 })
 export class NotFound {
 
+  goBack(): void {
+    window.history.back();
+  }
+
+  goHome(): void {
+    window.location.href = '/';
+  }
 }
