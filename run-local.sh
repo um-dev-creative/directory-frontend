632afcb8fcbad0125d5345db9120c701ea320a3b
omairys <hello.omug@gmail.com>
2026-02-19 09:36:32 -0500

    update run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -6,22 +6,21 @@
 set -e  # Salir si algún comando falla
 
 echo "🧹 Limpiando archivos de build anteriores..."
-# rm -rf dist/ .angular/
+rm -rf dist/ .angular/
 
 echo "🔑 Configurando variables de entorno..."
-export VAULT_TOKEN=hvs.CAESIOCJZG1cGAf0NYDjv-L2dcd5NvvAx8spIE0WT88_pqmVGh4KHGh2cy41bTNiN2YzMEhGMjJxUUJFQ2ExZ0ZzSDY
-export ENVM=qa-cloud
-export VAULT_URL=http://prx-qa.vault.tst
-# export ENVM=qa-cloud
+export VAULT_TOKEN=<tu_token_de_vault_aqui>
+export VAULT_URL=<url_de_vault_aqui>
+export ENVM=<entorno_aqui>
 
 echo "📋 Variables configuradas:"
 echo "VAULT_TOKEN: $VAULT_TOKEN"
-echo "ENVM: $ENVM"
 echo "VAULT_URL: $VAULT_URL"
+echo "ENVM: $ENVM"
 
-# echo "🏗️  Construyendo la aplicación SSR..."
-# npm run build:ssr
+echo "🏗️  Construyendo la aplicación SSR..."
+npm run build:ssr
 
 echo "🚀 Iniciando servidor de desarrollo..."
-npm run start-dev:unix
+npm run dev:unix
 echo "🌐 Accede a la aplicación en https://localhost:7001"
