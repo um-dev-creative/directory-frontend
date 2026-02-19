0826f7777ae917f05539779d644db4bcbe5a8d75
omairys <hello.omug@gmail.com>
2026-02-18 21:04:28 -0500

    chore: pre-tailwind-v4-migration snapshot

--- run-local.sh
+++ run-local.sh
@@ -6,20 +6,22 @@
 set -e  # Salir si algún comando falla
 
 echo "🧹 Limpiando archivos de build anteriores..."
-rm -rf dist/ .angular/
+# rm -rf dist/ .angular/
 
 echo "🔑 Configurando variables de entorno..."
-export VAULT_TOKEN=<tu_token_de_vault_aqui>
-export ENVM=qa
+export VAULT_TOKEN=hvs.CAESIOCJZG1cGAf0NYDjv-L2dcd5NvvAx8spIE0WT88_pqmVGh4KHGh2cy41bTNiN2YzMEhGMjJxUUJFQ2ExZ0ZzSDY
+export ENVM=qa-cloud
+export VAULT_URL=http://prx-qa.vault.tst
 # export ENVM=qa-cloud
 
 echo "📋 Variables configuradas:"
 echo "VAULT_TOKEN: $VAULT_TOKEN"
 echo "ENVM: $ENVM"
+echo "VAULT_URL: $VAULT_URL"
 
-echo "🏗️  Construyendo la aplicación SSR..."
-npm run build:ssr
+# echo "🏗️  Construyendo la aplicación SSR..."
+# npm run build:ssr
 
 echo "🚀 Iniciando servidor de desarrollo..."
-npm run dev:unix
-echo "🌐 Accede a la aplicación en http://localhost:7001"
+npm run start-dev:unix
+echo "🌐 Accede a la aplicación en https://localhost:7001"
